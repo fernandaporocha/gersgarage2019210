4a878669ef2e33accc6091c565091debfa9dbd5b
Fernanda Rocha <fernandaporocha@gmail.com>
2020-08-06 07:03:32 +0100

    Github deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://fernandarocha2019210.herokuapp.com/',
+  API_URL: 'https://fernandaporocha.github.io/gersgarage2019210/', //'https://fernandarocha2019210.herokuapp.com/',
   APP_URL: 'https://fernandarocha2019210api.herokuapp.com/'
 };
