a7e61dc883027ed9e4f7911440355cb9603be3f6
Fernanda Rocha <fernandaporocha@gmail.com>
2020-08-10 16:42:19 +0100

    Adding Login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
 import { Login } from '../login';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,17 @@ export class LoginComponent implements OnInit {
 
   invalidLogin = false
   login: Login = new Login()
+  authForm: FormGroup;
 
   constructor(private router: Router,
-    private loginService: AuthenticationService) { }
+    private loginService: AuthenticationService,
+    private fb: FormBuilder) { 
+      // use FormBuilder to create a form group
+    this.authForm = this.fb.group({
+      'username': ['', Validators.required],
+      'password': ['', Validators.required]
+    });
+    }
 
   ngOnInit() {
   }
