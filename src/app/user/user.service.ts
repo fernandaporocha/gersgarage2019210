43b67bafc9a810ff371e907fde3caf0155735d69
Fernanda Rocha <fernandaporocha@gmail.com>
2020-08-15 15:36:17 +0100

    Removing not used code

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from './User';
 import { environment } from 'src/environments/environment';
-import { post } from 'jquery';
-import { ApiService } from './../api.service'
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +10,21 @@ export class UserService {
 
   private baseUrl = `${environment.API_URL}user`;
 
-  constructor(private http:HttpClient,
-    private apiService: ApiService) { }
+  constructor(private http:HttpClient) { }
 
   getUserList(): Observable<any>{
-    console.log("getlist");
     return this.http.get(`${this.baseUrl}`);
   }
 
   getStaffList(): Observable<any>{
-    console.log("getStafflist");
     return this.http.get(`${this.baseUrl}/getStaff`);
   }
 
   createUser(user: object): Observable<object>{
-    console.log("create user service");
     return this.http.post(`${this.baseUrl}`, user);
   }
 
   createContactInfor(contactInfo: object): Observable<any>{
-    console.log('createContactInfor');
     return this.http.post(`${environment.API_URL}contactDetails`, contactInfo);
   }
 
@@ -49,9 +41,7 @@ export class UserService {
   }
 
   login(login: any): Observable<any> { 
-    console.log(login); 
-    //return this.http.post(`${this.baseUrl}/login`, login);
-    return this.apiService.post(`${this.baseUrl}/login`, login);  
+    return this.http.post(`${this.baseUrl}/login`, login);  
   } 
   
   updateUser(value: any): Observable<Object> {  
